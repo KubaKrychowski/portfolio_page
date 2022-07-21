05e1c853d150325c56d2e70a4bd8a29deb732e36
Kuba Krychowski <kuba.krychowski@mieszkanie.pl>
2022-07-21 18:52:11 +0200

    BUILD

--- portfolio/src/app/app.component.ts
+++ portfolio/src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'portfolio';
+
+  isCompanyDescriptionVisible: boolean = false;
+
+  toggleCompanyDescription(): void {
+    this.isCompanyDescriptionVisible = !this.isCompanyDescriptionVisible
+  }
 }
