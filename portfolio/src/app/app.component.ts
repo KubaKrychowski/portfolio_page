060d18432c53826029401aad84714cd54b897cc4
Kuba Krychowski <kuba.krychowski@mieszkanie.pl>
2022-07-21 21:33:01 +0200

    BUILD

--- portfolio/src/app/app.component.ts
+++ portfolio/src/app/app.component.ts
@@ -1,16 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'portfolio';
 
+  @ViewChild('carrerGoals', {static: false}) carrerGoals!: ElementRef<HTMLCanvasElement>;
+
+  public ctx!: CanvasRenderingContext2D | null;
+
   isCompanyDescriptionVisible: boolean = false;
 
+  ngAfterViewInit(): void {
+    this.ctx = this.carrerGoals.nativeElement.getContext('2d');
+    if(this.ctx){
+      this.ctx.beginPath();
+      this.ctx.lineTo(100,10);
+      this.ctx.stroke();
+    } else {
+      console.log('ctx is not defined');
+    }
+
+  }
+
   toggleCompanyDescription(): void {
-    this.isCompanyDescriptionVisible = !this.isCompanyDescriptionVisible
+    this.isCompanyDescriptionVisible = !this.isCompanyDescriptionVisible;
   }
 }
--- portfolio/src/app/app.module.ts
+++ portfolio/src/app/app.module.ts
@@ -4,15 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialsModule } from './materials/materials.module';
+import { SwiperModule } from 'swiper/angular';
+import { SwiperComponent } from './swiper/swiper.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SwiperComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MaterialsModule,
+    SwiperModule
   ],
   providers: [],
   bootstrap: [AppComponent]
