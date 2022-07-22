9fd4641b113c37feccaa0b6f27fb43b070888746
Kuba Krychowski <kuba.krychowski@mieszkanie.pl>
2022-07-22 07:38:51 +0200

    BUILD

--- portfolio/src/app/app.module.ts
+++ portfolio/src/app/app.module.ts
@@ -5,20 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialsModule } from './materials/materials.module';
-import { SwiperModule } from 'swiper/angular';
-import { SwiperComponent } from './swiper/swiper.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SwiperComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MaterialsModule,
-    SwiperModule
   ],
   providers: [],
   bootstrap: [AppComponent]
